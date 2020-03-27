9b53b045934c6e6cdc7a225ad5da156767a05731
Aliagho <ali.bahrami633@yahoo.com>
2020-03-28 10:42:29 +1100

    problem 31 solved

--- 31-Stu_Callback-Review/Unsolved/index.js
+++ 31-Stu_Callback-Review/Unsolved/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("animals.json", "utf8", function(err, data) {
+fs.readFile("animals.json", "utf8", function (err, data) {
   if (err) {
     throw err;
   }
@@ -11,4 +11,29 @@ fs.readFile("animals.json", "utf8", function(err, data) {
   // Create two new arrays to contain the cats and dogs objects
   const dogs = [];
   const cats = [];
+
+  animalJSON.forEach(function (animal) {
+    if (animal.species === "cat")
+      cats.push(animal)
+    else
+      dogs.push(animal);
+  });
+  console.log(cats);
+
+  const dogJSON = JSON.stringify(dogs, null, 2);
+  const catJSON = JSON.stringify(cats, null, 2);
+
+  // ...
+
+  fs.writeFile("dogs.json", dogJSON, function (err) {
+    if (err)
+      throw err;
+  });
+
+  fs.writeFile("cats.json", catJSON, function (err) {
+    if (err)
+      throw err;
+  });
+
+  console.log("Success!");
 });
